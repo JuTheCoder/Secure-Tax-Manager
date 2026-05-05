879e5ce677df0474ad394091cc61697d17164398
JuTheCoder <gonzalezmd0417@gmail.com>
2026-05-05 14:03:11 -0400

    chages

--- record.h
+++ record.h
@@ -3,12 +3,14 @@
 
 #define MAX_TEXT_SIZE 50
 
+// Defines the structure of one record
 typedef struct {
     char type[MAX_TEXT_SIZE];
     char category[MAX_TEXT_SIZE];
     float amount;
 } Record;
 
+// Stores multiple records. Uses dynamic memory so the list can grow as more records are added.
 typedef struct {
     Record *records;
     int size;
